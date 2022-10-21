e3853cbcd48faeb7ad251ac7be7ef8438d878042
motarek11 <mt5597633@gmail.com>
2022-10-21 17:37:11 +0200

    Non_Teachnecal Interstef : PR & Documentation

--- Mohamed.py
+++ Mohamed.py
@@ -1,8 +1,10 @@
-import opencv
+#import opencv
 
 my name = "mohamed tarek"
 my age = 19
 my department = "communication & electronics"
 My Graduation Year = 2026
 My favourit subteam = "software"
+techknecal intested = "Vesion & Navigation & maintanance"
+
 
